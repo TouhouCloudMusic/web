cfd06de803e017a7ee7929ce81ae6ca81685746e
狐狸 <134658521+Huliiiiii@users.noreply.github.com>
2024-05-18 14:28:51 +0800

    rename: defaultAppState => devAppState

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,14 @@
-import { MetaProvider, Title } from "@solidjs/meta";
-import { Router } from "@solidjs/router";
-import { FileRoutes } from "@solidjs/start/router";
-import { Suspense } from "solid-js";
-import "./app.css";
-import { AppStateProvider, defaultAppState } from "./states/app.state";
-import Header from "./components/Header/Header";
+import { MetaProvider, Title } from "@solidjs/meta"
+import { Router } from "@solidjs/router"
+import { FileRoutes } from "@solidjs/start/router"
+import { Suspense } from "solid-js"
+import "./app.css"
+import { AppStateProvider, devAppState } from "./state/app.state"
+import Header from "~/component/Header/Header"
 
 export default function App() {
 	return (
-		<AppStateProvider defaultState={defaultAppState}>
+		<AppStateProvider defaultState={devAppState}>
 			<Router
 				root={(props) => (
 					<MetaProvider>
@@ -20,5 +20,5 @@ export default function App() {
 				<FileRoutes />
 			</Router>
 		</AppStateProvider>
-	);
+	)
 }
