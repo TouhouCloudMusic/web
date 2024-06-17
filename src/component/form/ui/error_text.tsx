789045dbd5529c1f1aadeac17814dd5bc429c013
狐狸 <134658521+Huliiiiii@users.noreply.github.com>
2024-06-17 11:14:43 +0800

    update formui/errortext

--- src/component/form/ui/error_text.tsx
+++ src/component/form/ui/error_text.tsx
@@ -1,3 +1,17 @@
-export function ErrorText(props: { text: string }) {
-	return <p class="text-sm text-red-700">{props.text}</p>
+import { Show } from "solid-js"
+
+type ErrorTextProps = {
+	text?: string
+	showWhen?: boolean
+}
+
+export function ErrorText(props: ErrorTextProps) {
+	return (
+		<Show
+			when={
+				props.showWhen ?? (props.text !== undefined && props.text.length > 0)
+			}>
+			<p class="text-sm text-red-700">{props.text}</p>
+		</Show>
+	)
 }
