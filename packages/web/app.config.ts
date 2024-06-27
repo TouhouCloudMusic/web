40bd9c67a63d10ade729d041f4cd1922378ae5f1
狐狸 <134658521+Huliiiiii@users.noreply.github.com>
2024-06-27 21:44:42 +0800

    web: merge `vitest config` to `app config`

--- packages/web/app.config.ts
+++ packages/web/app.config.ts
@@ -1,10 +1,23 @@
-// @ts-check
+/// <reference types="vitest" />
 import { defineConfig } from "@solidjs/start/config"
 import viteConfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
 	vite: {
 		plugins: [viteConfigPaths()],
+		resolve: {
+			conditions: ["development", "browser"],
+		},
+		test: {
+			browser: {
+				enabled: true,
+				name: "chrome",
+				headless: true,
+			},
+			include: [
+				"test/dom/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}",
+			],
+		},
 	},
 	routeDir: "./route",
 })
