6454672129eb7e94c5379f9c476fcaed9c5c4afd
狐狸 <134658521+Huliiiiii@users.noreply.github.com>
2024-07-01 08:13:33 +0800

    add util for match unknown catch to error

---
+++ packages/web/src/lib/convert/match_unknown_to_error.ts
@@ -0,0 +1,9 @@
+export function matchUnknownToError(unknown: unknown) {
+	if (unknown instanceof Error) {
+		return unknown
+	} else if (typeof unknown === "string") {
+		return new Error(unknown)
+	} else {
+		return new Error("unknown error")
+	}
+}
