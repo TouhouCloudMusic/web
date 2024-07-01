95f32606ca6cdc7569dce2ff8532fd2c39b90239
狐狸 <134658521+Huliiiiii@users.noreply.github.com>
2024-07-01 10:24:16 +0800

    edit-artist-page(init_data): refactor initialize data and improve error handling

--- packages/web/src/route/(artist)/add/artist/init_data.ts
+++ packages/web/src/route/(artist)/add/artist/init_data.ts
@@ -1,18 +1,30 @@
-"use server"
-import { type Params, cache, redirect } from "@solidjs/router"
-import e from "@touhouclouddb/database"
+import { cache, redirect, type Params } from "@solidjs/router"
+import { taskEither } from "fp-ts"
+import { pipe } from "fp-ts/lib/function"
+import { matchUnknownToError } from "~/lib/convert/match_unknown_to_error"
 import { validateAndThrowRedirectEither } from "~/lib/validate/throw_redirect"
 import { isEmptyOrValidID } from "~/lib/validate/validate_params"
 import { findArtistByID_EditArtistPage } from "./db"
 
 export const initData = cache(async function (params: Params) {
+	"use server"
 	const id = validateAndThrowRedirectEither(isEmptyOrValidID, params)
 	if (!id) return
-	try {
-		return await findArtistByID_EditArtistPage(id)
-	} catch (error) {
-		// TODO: 研究如何把error发到客户端上
-		console.log(e)
-		throw redirect("/500")
-	}
+	const task = pipe(
+		taskEither.tryCatch(
+			() => findArtistByID_EditArtistPage(id),
+			(reason) => matchUnknownToError(reason)
+		),
+		taskEither.match(
+			() => {
+				// TODO: 研究如何把error发到客户端上
+				// TODO: 错误日志
+				throw redirect("/500")
+			},
+			(data) => data
+		)
+	)
+	const res = await task()
+	if (!res) throw redirect("/404")
+	else return res
 }, `init_edit_artist_page`)
