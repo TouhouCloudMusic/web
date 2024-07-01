ecc7ed27df1352e97d2aa45f5d94c5396050bffc
狐狸 <134658521+Huliiiiii@users.noreply.github.com>
2024-07-01 10:21:50 +0800

    edit-artist-page(db): refactor for better readability and type

--- packages/web/src/route/(artist)/add/artist/db.ts
+++ packages/web/src/route/(artist)/add/artist/db.ts
@@ -5,64 +5,66 @@ import { client } from "~/database/edgedb"
 export type ArtistByID_EditArtistPage = Awaited<
 	ReturnType<typeof findArtistByID_EditArtistPage>
 >
+
 export async function findArtistByID_EditArtistPage(id: string) {
-	const res = await e
-		.select(e.default.Artist, (artist) => ({
-			filter_single: {
-				app_id: e.int64(id),
+	const query = e.select(e.default.Artist, (artist) => ({
+		filter_single: {
+			app_id: e.int64(id),
+		},
+		...e.default.Artist["*"],
+		...e.is(e.Artist.Group, {
+			members: {
+				...e.Artist.Group.members["*"],
+				"@join_year": true,
+				"@leave_year": true,
 			},
-			...e.default.Artist["*"],
-			...e.is(e.Artist.Group, {
-				members: {
-					...e.Artist.Group.members["*"],
-					"@join_year": true,
-					"@leave_year": true,
-				},
-			}),
-			...e.is(e.Artist.Group, {
-				str_members: {
+		}),
+		...e.is(e.Artist.Group, {
+			str_members: {
+				id: true,
+				name: true,
+				join_year: true,
+				leave_year: true,
+			},
+		}),
+		...e.is(e.Artist.Person, {
+			member_of: {
+				...e.Artist.Person.member_of["*"],
+				members: (members) => ({
+					filter_single: e.op(members.id, "=", artist.id),
 					id: true,
-					name: true,
-					join_year: true,
-					leave_year: true,
-				},
-			}),
-			...e.is(e.Artist.Person, {
-				member_of: {
-					...e.Artist.Person.member_of["*"],
-					members: (members) => ({
-						filter_single: e.op(members.id, "=", artist.id),
-						id: true,
-						join_year: members["@join_year"],
-						leave_year: members["@leave_year"],
-					}),
-				},
-			}),
-			...e.is(e.Artist.Person, {
-				str_member_of: {
-					id: true,
-					name: true,
-					join_year: true,
-					leave_year: true,
-				},
-			}),
-		}))
-		.run(client)
-	if (!res) return
-	return {
-		...res,
-		member_of:
-			res.member_of?.map((m) => ({
-				...m,
-				"@join_year":
-					m.members.filter((person) => person.id === res.id)[0].join_year ??
-					null,
-				"@leave_year":
-					m.members.filter((person) => person.id === res.id)[0].leave_year ??
-					null,
-				members: null,
-			})) ?? null,
-	}
+					join_year: members["@join_year"],
+					leave_year: members["@leave_year"],
+				}),
+			},
+		}),
+		...e.is(e.Artist.Person, {
+			str_member_of: {
+				id: true,
+				name: true,
+				join_year: true,
+				leave_year: true,
+			},
+		}),
+	}))
+
+	const res = await query.run(client)
+	return !res ? null : (
+			{
+				...res,
+				member_of:
+					res.member_of?.map((m) => ({
+						...m,
+						"@join_year":
+							m.members.filter((person) => person.id === res.id)[0].join_year ??
+							null,
+						"@leave_year":
+							m.members.filter((person) => person.id === res.id)[0]
+								.leave_year ?? null,
+						members: null,
+					})) ?? null,
+			}
+		)
 }
 
 export type ArtistListByKeyword_EditArtistPage = Awaited<
