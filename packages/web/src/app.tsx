80cbb20dd9e3d3db0b738e7114d9bffcbbb66e60
狐狸 <134658521+Huliiiiii@users.noreply.github.com>
2024-07-07 07:51:17 +0800

    show custom error page only in production
    - add font display to custom fonts

--- packages/web/src/app.tsx
+++ packages/web/src/app.tsx
@@ -1,7 +1,7 @@
 import { MetaProvider, Title } from "@solidjs/meta"
 import { Router } from "@solidjs/router"
 import { FileRoutes } from "@solidjs/start/router"
-import { ErrorBoundary, Suspense, createResource } from "solid-js"
+import { ErrorBoundary, Show, Suspense, createResource } from "solid-js"
 import Header from "~/component/header/header"
 import "./app.css"
 import ErrorPage from "./route/500"
@@ -26,24 +26,27 @@ export default () => {
 	const [serverTheme] = createResource(() => getCookieTheme())
 	const initTheme = parseInt(serverTheme()?.toString() ?? "0", 10)
 	return (
-		// <Suspense fallback={<div>Loading...</div>}>
 		<MetaProvider>
 			<AppStateProvider
 				defaultState={{
 					...devAppState,
 					theme: initTheme,
 				}}>
-				<ErrorBoundary
-					fallback={(e: Error) => (
-						<>
-							{/* <Header /> */}
-							<ErrorPage msg={`${e.stack}`} />
-						</>
-					)}>
+				<Show
+					when={import.meta.env.DEV}
+					fallback={
+						<ErrorBoundary
+							fallback={(e: Error) => (
+								<>
+									<ErrorPage msg={e.stack} />
+								</>
+							)}>
+							<App />
+						</ErrorBoundary>
+					}>
 					<App />
-				</ErrorBoundary>
+				</Show>
 			</AppStateProvider>
 		</MetaProvider>
-		// </Suspense>
 	)
 }
