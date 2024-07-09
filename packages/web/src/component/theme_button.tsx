c7c8ecaf025de7f190323c65546327bbe8b76ee1
狐狸 <134658521+Huliiiiii@users.noreply.github.com>
2024-07-10 00:20:53 +0800

    fix theme button style

--- packages/web/src/component/theme_button.tsx
+++ packages/web/src/component/theme_button.tsx
@@ -1,24 +1,25 @@
 import { SunIcon, MoonIcon } from "solid-radix-icons"
 import { Switch, Match } from "solid-js"
 import { AppTheme, useAppState } from "~/state/app_state"
+import { Button } from "./button"
 
 export function ThemeButton(props: { class?: string }) {
 	const { theme, setTheme } = useAppState()
 	return (
 		<Switch>
 			<Match when={theme() === AppTheme.light}>
-				<button
-					class={`button flex place-content-center items-center ${props.class}`}
+				<Button.Borderless
+					class={`flex place-content-center items-center ${props.class}`}
 					onClick={() => setTheme(AppTheme.dark)}>
 					<SunIcon />
-				</button>
+				</Button.Borderless>
 			</Match>
 			<Match when={theme() === AppTheme.dark}>
-				<button
-					class={`button flex place-content-center items-center ${props.class}`}
+				<Button.Borderless
+					class={`flex place-content-center items-center ${props.class}`}
 					onClick={() => setTheme(AppTheme.light)}>
 					<MoonIcon />
-				</button>
+				</Button.Borderless>
 			</Match>
 		</Switch>
 	)
