d0cf9e878feb23b0cddef27f9ed937506c9ff200
狐狸 <134658521+Huliiiiii@users.noreply.github.com>
2024-07-15 03:35:59 +0800

    refactor app.tsx

--- packages/web/src/app.tsx
+++ packages/web/src/app.tsx
@@ -1,11 +1,11 @@
 import { Title } from "@solidjs/meta"
 import { Router } from "@solidjs/router"
 import { FileRoutes } from "@solidjs/start/router"
-import { ErrorBoundary, Show, Suspense } from "solid-js"
+import { ErrorBoundary, type ParentProps, Show, Suspense } from "solid-js"
 import Header from "~/component/header/header"
 import "./app.css"
 import ErrorPage from "./route/500"
-import { I18NTranstionStyle, useI18N } from "./state/i18n.tsx"
+import { I18NTranstionStyle, useI18N } from "./state/i18n"
 import { Providers } from "./state/index.tsx"
 
 function Routes() {
@@ -15,7 +15,7 @@ function Routes() {
 				<>
 					<Title>Doujin Cloud DB</Title>
 					<Header />
-					<Suspense>{props.children}</Suspense>
+					{props.children}
 				</>
 			)}>
 			<FileRoutes />
@@ -23,35 +23,39 @@ function Routes() {
 	)
 }
 
-function Wrapper() {
+function CustomErrorBoundary(props: ParentProps) {
 	return (
-		<div
-			style={{
-				...(useI18N().duringTransition() ? I18NTranstionStyle : {}),
-			}}
-			id="app_wrapper">
-			<Show
-				when={import.meta.env.DEV}
-				fallback={
-					<ErrorBoundary
-						fallback={(e: Error) => (
-							<>
-								<ErrorPage msg={e.stack} />
-							</>
-						)}>
-						<Routes />
-					</ErrorBoundary>
-				}>
-				<Routes />
-			</Show>
-		</div>
+		<Show
+			when={import.meta.env.DEV}
+			fallback={
+				<ErrorBoundary
+					fallback={(e: Error) => (
+						<>
+							<ErrorPage msg={e.stack} />
+						</>
+					)}>
+					{props.children}
+				</ErrorBoundary>
+			}>
+			{props.children}
+		</Show>
 	)
 }
 
-export default () => {
+export default function App() {
 	return (
-		<Providers>
-			<Wrapper />
-		</Providers>
+		<Suspense>
+			<CustomErrorBoundary>
+				<Providers>
+					<div
+						style={{
+							...(useI18N().duringTransition() ? I18NTranstionStyle : {}),
+						}}
+						id="app_wrapper">
+						<Routes />
+					</div>
+				</Providers>
+			</CustomErrorBoundary>
+		</Suspense>
 	)
 }
