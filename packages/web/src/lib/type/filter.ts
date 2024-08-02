a36b0b6fb79335518f8edf1a3e115bb8c5b9449d
狐狸 <134658521+Huliiiiii@users.noreply.github.com>
2024-08-02 11:27:57 +0800

    add new Error `NotImplementedError`
    - improve type for filter util

--- packages/web/src/lib/type/filter.ts
+++ packages/web/src/lib/type/filter.ts
@@ -1,6 +1,6 @@
 export type FilterRecord<T> = Partial<Record<keyof T, boolean>>
 
-export type Filter<TData, TFilter> = Pick<
+export type Filter<TData, TFilter extends Record<PropertyKey, boolean>> = Pick<
 	TData,
 	{
 		[K in keyof TData]: K extends keyof TFilter ?
