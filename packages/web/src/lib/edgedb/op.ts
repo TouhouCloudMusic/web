12c7b8f7690811f8f844f84787f7888ba8288dfd
狐狸 <134658521+Huliiiiii@users.noreply.github.com>
2024-08-30 19:45:46 +0800

    update helper lib

--- packages/web/src/lib/edgedb/op.ts
+++ packages/web/src/lib/edgedb/op.ts
@@ -1,32 +1,39 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import e from "@touhouclouddb/database"
-import { type $expr_Operator } from "@touhouclouddb/database/reflection.js"
+import {
+	type $expr_Operator,
+	type BaseType,
+	type Cardinality,
+} from "@touhouclouddb/database/reflection.js"
 
-type exprs = ($expr_Operator<any, any> | boolean)[]
-export function combineOp(
+export function combine_op(
 	operator: string,
-	...exprs: exprs
+	...exprs: $expr_Operator<any, any>[]
 ): $expr_Operator<any, any> | undefined {
 	if (exprs.length === 0) {
 		return undefined
 	}
 
 	if (exprs.length === 1) {
-		return exprs[0] as $expr_Operator<any, any>
+		return exprs[0]
 	}
 
 	return e.op(
 		// @ts-expect-error
 		exprs[0],
 		operator as never,
-		combineOp(operator, ...exprs.slice(1))
+		combine_op(operator, ...exprs.slice(1))
 	)
 }
 
-export function op_and(...exprs: exprs): $expr_Operator<any, any> | undefined {
-	return combineOp("and", ...exprs)!
+export function op_and<T extends BaseType, U extends Cardinality>(
+	...exprs: $expr_Operator<T, U>[]
+): $expr_Operator<T, U> {
+	return combine_op("and", ...exprs) as $expr_Operator<T, U>
 }
 
-export function op_or(...exprs: exprs): $expr_Operator<any, any> | undefined {
-	return combineOp("or", ...exprs)!
+export function op_or<T extends BaseType, U extends Cardinality>(
+	...exprs: $expr_Operator<T, U>[]
+): $expr_Operator<T, U> {
+	return combine_op("or", ...exprs) as $expr_Operator<T, U>
 }
