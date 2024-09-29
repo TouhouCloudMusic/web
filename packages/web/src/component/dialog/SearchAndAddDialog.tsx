1a203fa05cfee278a7facab25dcd5043101220df
狐狸 <134658521+Huliiiiii@users.noreply.github.com>
2024-09-29 14:01:57 +0800

    refactor `SearchAndAddDialog`

--- packages/web/src/component/dialog/SearchAndAddDialog.tsx
+++ packages/web/src/component/dialog/SearchAndAddDialog.tsx
@@ -1,25 +1,29 @@
-import { For, JSX } from "solid-js"
+import { For, type JSX, type ParentProps } from "solid-js"
 import { Cross2Icon, MagnifyingGlassIcon, PlusIcon } from "solid-radix-icons"
 import { twMerge } from "tailwind-merge"
 import { TertiaryButton } from "../button/index.tsx"
 import { Dialog } from "../form/Dialog/index.ts"
 import { TextField } from "../form/index.tsx"
 
-export function SearchAndAddDialog<T extends Record<string, unknown>>(props: {
-	// onSearch: (args: string) => unknown
-	data: T[]
-	// setData: (args: T) => unknown
-	ListItem: (props: { data: T }) => JSX.Element
-}) {
+export function SearchAndAddDialog<T extends Record<string, unknown>>(
+	props: ParentProps & {
+		onSubmit?: (args?: T) => unknown
+		// onSearch: (args: string) => unknown
+		data: T[]
+		// setData: (args: T) => unknown
+		defaultOpen?: boolean
+		listItem: (props: { data: T }) => JSX.Element
+		title: JSX.Element
+	}
+) {
 	const onSubmit = (e: Event) => {
 		e.preventDefault()
-		// @ts-ignore
-		props.onInsert()
+		props.onSubmit?.()
 	}
 
 	return (
 		<Dialog.Layout
-			defaultOpen
+			defaultOpen={props.defaultOpen ?? false}
 			trigger={
 				<TertiaryButton
 					size="xs"
@@ -27,55 +31,38 @@ export function SearchAndAddDialog<T extends Record<string, unknown>>(props: {
 					<PlusIcon />
 				</TertiaryButton>
 			}>
-			<Dialog.Content class="flex max-h-[75%] min-h-[32rem] max-w-sm flex-col px-0 pb-8">
+			<Dialog.Content class="flex max-h-[min(48rem,50%)] min-h-[32rem] max-w-sm flex-col px-0 pb-8">
 				<div class="mx-4 flex justify-between">
-					<Dialog.Title class="text-lg font-medium">Add Alias</Dialog.Title>
-					<Dialog.CloseButton class="p-1">
-						<Cross2Icon />
+					<Dialog.Title class="text-lg font-medium">{props.title}</Dialog.Title>
+					<Dialog.CloseButton class="aspect-square h-full p-1">
+						<Cross2Icon class="m-auto" />
 					</Dialog.CloseButton>
 				</div>
-				<form
-					onSubmit={onSubmit}
-					class="relative mx-4 mt-3 grid grid-rows-1">
-					<div class="absolute w-8 self-center">
-						<MagnifyingGlassIcon class="mx-auto size-4" />
-					</div>
-					<input
-						class={twMerge(
-							TextField.Input.className,
-							`border-none bg-slate-100 pl-8 ring-0`
-						)}
-					/>
-				</form>
+				<div class="mx-4">
+					<form
+						onSubmit={onSubmit}
+						class="relative mb-6 mt-3 grid grid-rows-1">
+						<MagnifyingGlassIcon class="text-tertiary absolute mx-auto size-4 w-8 self-center" />
+						<input
+							class={twMerge(
+								TextField.Input.className,
+								`border-0 bg-slate-100 pl-8 ring-0`
+							)}
+						/>
+					</form>
+				</div>
 
-				<ul class="bg-secondary mt-6 flex w-full grow flex-col overflow-auto border-y-[1.5px] border-slate-100 *:min-h-16">
+				<ul class="bg-secondary border-y-1.5 overflow-auto border-slate-200">
 					<For each={props.data}>
 						{(data) => (
-							<li class="bg-primary flex size-5 w-full justify-between border-y-[1.5px] border-slate-100 px-4 *:self-center">
-								<props.ListItem data={data} />
+							<li class="bg-primary border-y-1.5 border-slate-200 px-4">
+								<props.listItem data={data} />
 							</li>
 						)}
 					</For>
-
-					{/* {fakeData.map((x) => (
-						<li class="bg-primary flex size-5 w-full justify-between border-y-[1.5px] border-slate-100 px-4 *:self-center">
-							<div>
-								<p>{x.name}</p>
-								<span class="text-tertiary">lorem ipsum</span>
-							</div>
-							<TertiaryButton
-								class="aspect-square min-h-6"
-								onClick={() =>
-									addAlias({
-										name: x.name,
-										is_str: false,
-									})
-								}>
-								<PlusIcon class="m-auto" />
-							</TertiaryButton>
-						</li>
-					))} */}
 				</ul>
+
+				{props.children}
 			</Dialog.Content>
 		</Dialog.Layout>
 	)
