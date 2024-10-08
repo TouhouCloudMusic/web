edde34fae3a996314963df8ba3a9c618186888f5
狐狸 <134658521+Huliiiiii@users.noreply.github.com>
2024-10-08 09:36:13 +0800

    搬数据库到服务器上

--- packages/server/db/test/index.ts
+++ packages/server/db/test/index.ts
@@ -1,18 +1,15 @@
-import { drizzle } from "drizzle-orm/postgres-js"
-import postgres from "postgres"
+import { drizzle } from "drizzle-orm/connect"
 import * as schema from "../schema"
 
 const { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER } = process.env
 
-const host = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`
+const url = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`
 
-console.log(`🐛 Connected database at"${host}`)
-
-const connection = postgres(
-	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-	{ connect_timeout: 10000 }
-)
-
-export const db = drizzle(connection, {
+export const db = await drizzle("postgres-js", {
+	connection: {
+		url,
+	},
 	schema,
 })
+
+console.log(`🐛 Connected database at"${url}`)
