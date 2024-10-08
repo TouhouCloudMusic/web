cc2309c0f1eb21fe7c15a6be7b600da2b843701d
狐狸 <134658521+Huliiiiii@users.noreply.github.com>
2024-10-08 08:57:23 +0800

    Fix wrong localized name schema

--- packages/server/db/type/typebox/index.ts
+++ packages/server/db/type/typebox/index.ts
@@ -22,7 +22,7 @@ const gen_out = createSelectSchema(artist, {
 	text_alias: text_alias_schema,
 })
 const link_props_out = t.Object({
-	localized_name: i18n_name_out,
+	localized_name: t.Optional(t.Array(i18n_name_out)),
 })
 export const artist_schema = t.Composite([gen_out, link_props_out])
 export type Artist = typeof artist_schema.static
@@ -41,7 +41,7 @@ const gen_in = createInsertSchema(artist, {
 })
 
 const link_props_in = t.Object({
-	localized_name: i18n_name_in,
+	localized_name: t.Optional(t.Array(i18n_name_in)),
 })
 export const new_artist_schema = t.Composite([gen_in, link_props_in])
 
