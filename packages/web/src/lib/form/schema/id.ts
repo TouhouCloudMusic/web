5dde6acf5bc931a8f9a944af7d167c23b6dc69ca
狐狸 <134658521+Huliiiiii@users.noreply.github.com>
2024-10-12 17:48:22 +0800

    Chore (#65)
    - Disable pre push hooks
    - Some other refactors

--- packages/web/src/lib/form/schema/id.ts
+++ packages/web/src/lib/form/schema/id.ts
@@ -1,3 +1,5 @@
 import * as v from "valibot"
 
-export const OptionalIDSchema = v.optional(v.string())
+export const OptionalIDSchema = v.optional(v.pipe(v.string(), v.uuid()))
+
+export const OptionalAppIDSchema = v.optional(v.pipe(v.number(), v.integer()))
---
+++ packages/web/src/lib/type/utils.ts
@@ -0,0 +1,7 @@
+export type SafePick<T, K extends keyof T> = {
+	[P in K]: T[P]
+}
+
+export type SafeOmit<T, K extends keyof T> = {
+	[P in keyof T as P extends K ? never : P]: T[P]
+}
