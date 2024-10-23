c14fa282e718c3020d1fe3c0710a2ad1bd1b8dbb
狐狸 <134658521+Huliiiiii@users.noreply.github.com>
2024-10-23 14:14:06 +0800

    Remove image on delete

--- packages/server/src/model/image.ts
+++ packages/server/src/model/image.ts
@@ -3,6 +3,7 @@ import { eq } from "drizzle-orm"
 import fs from "fs/promises"
 import { image_table } from "~/database/schema"
 import { DB, db } from "~/service/database"
+
 export class ImageModel {
   private db: DB
   constructor(_db?: DB) {
@@ -10,33 +11,52 @@ export class ImageModel {
   }
 
   private async writeFile(name: string, bytes: Uint8Array) {
-    return await fs.writeFile(`${process.env.IMAGE_PATH}/${name}`, bytes)
+    const path = `${process.env.IMAGE_PATH}/${name}`
+    if (process.isBun) return await Bun.write(path, bytes)
+    else return await fs.writeFile(path, bytes)
+  }
+
+  private async removeFile(name: string) {
+    const path = `${process.env.IMAGE_PATH}/${name}`
+    return await fs.unlink(path)
   }
 
   async upload(user_id: number, image: File) {
     const bytes = await image.bytes()
+    const extension_name = image.name.split(".").pop()
     const image_hash = smartMd5(bytes)
-    const { id: image_id } = (
-      await this.db
-        .insert(image_table)
-        .values({
-          filename: image_hash,
-          uploaded_by: user_id,
-        })
-        .returning({ id: image_table.id })
-    )[0]
-    await this.writeFile(image_hash, bytes)
+    const file_name = `${image_hash}.${extension_name}`
+    const [{ image_id }] = await this.db
+      .insert(image_table)
+      .values({
+        filename: file_name,
+        uploaded_by: user_id,
+      })
+      .returning({ image_id: image_table.id })
+    await this.writeFile(file_name, bytes)
 
     return image_id
   }
 
   async deleteByHash(image_hash: string) {
-    return await this.db
+    const [{ filename }] = await this.db
       .delete(image_table)
       .where(eq(image_table.filename, image_hash))
+      .returning({
+        filename: image_table.filename,
+      })
+
+    await this.removeFile(filename)
   }
 
   async deleteByID(id: number) {
-    return await this.db.delete(image_table).where(eq(image_table.id, id))
+    const [{ filename }] = await this.db
+      .delete(image_table)
+      .where(eq(image_table.id, id))
+      .returning({
+        filename: image_table.filename,
+      })
+
+    await this.removeFile(filename)
   }
 }
