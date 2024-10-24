c33a1c6a386604306b2fffa2f3dfeedda00bac99
狐狸 <134658521+Huliiiiii@users.noreply.github.com>
2024-10-24 21:04:59 +0800

    Add role to user links schema

--- packages/server/src/database/user/typebox.ts
+++ packages/server/src/database/user/typebox.ts
@@ -3,17 +3,20 @@ import { createInsertSchema, createSelectSchema } from "drizzle-typebox"
 import { t } from "elysia"
 import { mapValues } from "radash"
 import { image_schema } from "../image/typebox"
-import { session, user } from "./schema"
+import { role_table, session, user } from "./schema"
 
 export const user_schema = createSelectSchema(user)
+export const user_role_schema = createSelectSchema(role_table)
 export const user_links_schema = t.Object(
   mapValues(
     {
       avatar: image_schema,
+      role: user_role_schema,
     },
     t.Nullable,
   ),
 )
+
 export type User = typeof user_schema.static
 export type UserLinks = typeof user_links_schema.static
 
