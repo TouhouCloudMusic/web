12b8031d435cd748bc3d059d66cdbb78debc4b1d
狐狸 <134658521+Huliiiiii@users.noreply.github.com>
2024-10-25 18:10:09 +0800

    Remove eslint plugin export scope because it causes vscode crash

--- packages/server/eslint.config.js
+++ packages/server/eslint.config.js
@@ -1,14 +1,11 @@
 import pluginJs from "@eslint/js"
 import prettier from "eslint-config-prettier"
-import exportScope from "eslint-plugin-export-scope"
 import oxlint from "eslint-plugin-oxlint"
 import globals from "globals"
 import tseslint from "typescript-eslint"
-
 export default tseslint.config(
   pluginJs.configs.recommended,
   prettier,
-  exportScope.configs.flatConfigRecommended,
   ...tseslint.configs.strictTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
   oxlint.configs["flat/recommended"],
