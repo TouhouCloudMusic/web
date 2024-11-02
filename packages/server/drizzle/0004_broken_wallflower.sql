45eec75b970e41859a719025cf1f5224f2abd19e
狐狸 <134658521+Huliiiiii@users.noreply.github.com>
2024-11-03 03:41:56 +0800

    Fix migration

--- packages/server/drizzle/0004_broken_wallflower.sql
+++ packages/server/drizzle/0004_broken_wallflower.sql
@@ -1,13 +1,14 @@
-DROP TABLE "release_type";--> statement-breakpoint
-ALTER TABLE "release_track_credit" RENAME TO "release_track_artist";--> statement-breakpoint
 ALTER TABLE "release" DROP CONSTRAINT "release_release_type_release_type_id_fk";
 --> statement-breakpoint
+DROP TABLE "release_type";--> statement-breakpoint
+CREATE TYPE "public"."release_type" AS ENUM('Album', 'EP', 'Single');--> statement-breakpoint
+ALTER TABLE "release_track_credit" RENAME TO "release_track_artist";--> statement-breakpoint
 ALTER TABLE "release_track_artist" DROP CONSTRAINT "release_track_credit_release_track_id_release_track_id_fk";
 --> statement-breakpoint
 ALTER TABLE "release_track_artist" DROP CONSTRAINT "release_track_credit_artist_id_artist_id_fk";
 --> statement-breakpoint
-ALTER TABLE "queue" ALTER COLUMN "new_data" SET DATA TYPE jsonb[];--> statement-breakpoint
-ALTER TABLE "release" ALTER COLUMN "release_type" SET DATA TYPE release_type;--> statement-breakpoint
+ALTER TABLE "queue" DROP COLUMN "new_data";--> statement-breakpoint
+ALTER TABLE "queue" ADD COLUMN "new_data" jsonb[];--> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "release_track_artist" ADD CONSTRAINT "release_track_artist_release_track_id_release_track_id_fk" FOREIGN KEY ("release_track_id") REFERENCES "public"."release_track"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
