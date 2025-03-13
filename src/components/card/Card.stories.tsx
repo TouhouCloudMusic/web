08b3ee738121bd93d0251ce7f115c239b80acb6b
Huli <134658521+Huliiiiii@users.noreply.github.com>
2025-03-13 22:09:40 +0800

    comp: Add `Card`

---
+++ src/components/card/Card.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from "storybook-solidjs"
+import { Card } from "."
+import { StoryLayout } from "~/utils/storybook_ext"
+
+const meta: Meta<typeof Card> = {
+  component: Card,
+  parameters: {
+    layout: StoryLayout.Centered,
+  },
+  tags: ["autodocs"],
+  argTypes: {},
+}
+
+export default meta
+type Story = StoryObj<typeof Card>
+
+export const Default: Story = {
+  render: () => (
+    <Card class="font-inter w-96 m-auto">
+      <h1 class="font-semibold text-2xl">This is a card</h1>
+      <article class=" text-secondary">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non eveniet,
+        ratione porro voluptates dolor eligendi error consequuntur cum provident
+        unde odio recusandae, optio adipisci! Obcaecati, fugit laboriosam. Aut,
+        expedita pariatur!
+      </article>
+    </Card>
+  ),
+}
