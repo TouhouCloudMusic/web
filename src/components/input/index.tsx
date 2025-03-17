b21aefef88e9fa2f5ad107e96bfa96f13577e84b
Huli <134658521+Huliiiiii@users.noreply.github.com>
2025-03-18 00:03:02 +0800

    comp: Add input

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,1 +1,23 @@
-export * from "./text_input.tsx"
+import { createMemo, type JSX, splitProps } from "solid-js"
+import { twMerge } from "tailwind-merge"
+
+export type Props = Omit<JSX.InputHTMLAttributes<HTMLInputElement>, "type">
+export function Input(props: Props) {
+  const CLASS = `
+		rounded-sm border border-gray-300 pl-2 h-8
+		hover:outline-1 outline-reimu-500
+		focus:outline-2
+		focus:outline-reimu-600
+		`
+
+  let [, other_props] = splitProps(props, ["class"])
+  let tw_class = createMemo(() => twMerge(CLASS, props.class))
+
+  return (
+    <input
+      type="text"
+      class={tw_class()}
+      {...other_props}
+    />
+  )
+}
