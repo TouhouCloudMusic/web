950113170bd9761190426e605ef0ca9e2cab1539
Huli <134658521+Huliiiiii@users.noreply.github.com>
2025-03-18 00:14:11 +0800

    comp: Tweak input style

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -4,11 +4,12 @@ import { twMerge } from "tailwind-merge"
 export type Props = Omit<JSX.InputHTMLAttributes<HTMLInputElement>, "type">
 export function Input(props: Props) {
   const CLASS = `
-		rounded-sm border border-gray-300 pl-2 h-8
-		hover:outline-1 outline-reimu-500
-		focus:outline-2
-		focus:outline-reimu-600
-		`
+    rounded-sm border border-gray-300 pl-2 h-8
+    transition-all duration-200
+    outline-1 outline-transparent
+    hover:outline-reimu-500
+    focus:outline-[1.5px] focus:outline-reimu-600
+    `
 
   let [, other_props] = splitProps(props, ["class"])
   let tw_class = createMemo(() => twMerge(CLASS, props.class))
