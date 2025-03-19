cff1a4c92699889f36975b8242a06e8e0d4fdd96
Huli <134658521+Huliiiiii@users.noreply.github.com>
2025-03-19 10:02:21 +0800

    Not found page

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,17 +5,23 @@ import { TanStackRouterDevtools } from "@tanstack/solid-router-devtools"
 import { ErrorBoundary, type ParentProps } from "solid-js"
 import { Header } from "~/components/header"
 import ErrorPage from "~/route/500.tsx"
+import { NotFound } from "~/views/NotFound"
 
 export const Route = createRootRoute({
-  component: () => (
+  component: RouteTree,
+  notFoundComponent: NotFound,
+})
+
+function RouteTree() {
+  return (
     <Layout>
       <AppErrorBoundary>
         <Outlet />
         <TanStackRouterDevtools />
       </AppErrorBoundary>
     </Layout>
-  ),
-})
+  )
+}
 
 function Layout(props: ParentProps) {
   // TODO: Need fix, transition bettwen language change
