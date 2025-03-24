0562cb46d3ae8a93509eb9e218917782b21953f6
Huli <134658521+Huliiiiii@users.noreply.github.com>
2025-03-24 20:20:53 +0800

    Pretty not found page

--- src/views/NotFound.tsx
+++ src/views/NotFound.tsx
@@ -2,9 +2,10 @@ import { Title } from "@solidjs/meta"
 
 export function NotFound() {
   return (
-    <div class="w-[64rem] py-32">
-      <Title>Not Found</Title>
+    <div class="size-full py-32 flex">
+      <Title>404 Not Found</Title>
       <img
+        class="w-3/4 m-auto"
         src="/img/status_code/404.png"
         alt="404 Not Found"
       />
