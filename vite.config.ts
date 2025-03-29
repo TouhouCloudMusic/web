b6e0993795d79d60f02b56999edda5b7ec011295
Huli <134658521+Huliiiiii@users.noreply.github.com>
2025-03-29 23:06:18 +0800

    Get constants from server

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { defineConfig } from "vite"
 import solidPlugin from "vite-plugin-solid"
 import tsconfigPaths from "vite-tsconfig-paths"
 
+import { GenerateCounstantsPlugin } from "./plugins/generate_constants"
+
 export default defineConfig({
   plugins: [
     solidPlugin(),
@@ -12,6 +14,7 @@ export default defineConfig({
     tailwindcss(),
     TanStackRouterVite({ target: "solid", autoCodeSplitting: true }),
     tsconfigPaths(),
+    GenerateCounstantsPlugin("http://localhost:12345"),
   ],
   server: {
     port: 3000,
