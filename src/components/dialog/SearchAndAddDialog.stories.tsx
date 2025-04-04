150d076437e0bc134bce806214d8fafee059805e
sblzdddd <101245833+sblzdddd@users.noreply.github.com>
2025-04-05 00:02:53 +0800

    Create SearchAndAddDialog.stories.tsx
    feat?: SearchAndAddDialog storybook example

---
+++ src/components/dialog/SearchAndAddDialog.stories.tsx
@@ -0,0 +1,40 @@
+import { createSignal } from "solid-js";
+import { type Meta, type StoryObj } from "storybook-solidjs";
+import { SearchAndAddDialog } from "./SearchAndAddDialog";
+
+const meta = {
+  component: SearchAndAddDialog,
+  tags: ["autodocs"],
+  argTypes: {
+    title: { control: "text", description: "Dialog title" },
+    data: { control: "object", description: "Data to display in the list" },
+    onSubmit: { action: "submitted", description: "Submit callback" },
+    listItem: { description: "Function to render list item" },
+  },
+} satisfies Meta<typeof SearchAndAddDialog>;
+
+export default meta;
+type Story = StoryObj<typeof SearchAndAddDialog>;
+
+// 示例
+export const Default: Story = {
+  render: (args) => {
+    const [open] = createSignal(false);
+    const listItem = (props: { data: { name: string } }) => <div>{props.data.name}</div>;
+
+    return (
+      <>
+        <SearchAndAddDialog
+          {...args}
+          listItem={listItem}
+          defaultOpen={open()}
+          onSubmit={() => alert("Submitted")}
+        />
+      </>
+    );
+  },
+  args: {
+    title: "添加物品",
+    data: [{ name: "Item 1" }, { name: "Item 2" }]
+  },
+}; 
