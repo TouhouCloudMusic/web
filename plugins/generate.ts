fbdc2c888a7be35b82ec2dd33f7de6d18b514361
Huli <134658521+Huliiiiii@users.noreply.github.com>
2025-04-04 19:12:10 +0800

    Fix constant generation url

--- plugins/generate.ts
+++ plugins/generate.ts
@@ -30,7 +30,12 @@ export const generatePlugin = (base_url?: string): Plugin => ({
 })
 
 async function generateConstants(base_url: string) {
-  const response = await fetch(`${base_url}/constant.ts`)
+  let url = new URL("constant.ts", base_url)
+  let response = await fetch(url)
+  if (!response.ok) {
+    throw new Error(`Failed to fetch ${url}: ${response.statusText}`)
+  }
+
   const content = await response.text()
 
   return fs.writeFile("src/constant/server.ts", content)
