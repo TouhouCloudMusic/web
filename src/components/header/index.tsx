adea2ca6170648758503c50db9113e74ba7fb249
xiaochen17 <sinoces4097@gmail.com>
2025-04-10 04:43:17 +0800

    Revert "header修改"
    This reverts commit 14d6984e497c998a955d0b754521fa937fa38cb3.

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -9,20 +9,18 @@ import { createClickOutside } from "~/utils/createClickOutside"
 
 import { Avatar } from "../avatar/index.tsx"
 import { Dialog } from "../dialog"
-/*import {
+import {
   BellAlertIcon,
   BellIcon,
   BellSlashIcon,
-} from "../icons/heroicons/24/outline.tsx"*/
- //禁用模板icon
+} from "../icons/heroicons/24/outline.tsx"
 import { Input } from "../input"
 import { RightSidebar } from "../sidebar/right"
-import  icon  from "../../assets/image/icon_s.png"
 
 const HEADER_BTN_CLASS = "size-6 p-1 m-auto"
 
 export function Header() {
-  const Divider = () => <span class="w-[0.5px] bg-slate-400 h-5 ml-3"></span> //分隔线 按Figma上的示例图取消使用，保留代码供未来修改调用
+  const Divider = () => <span class="w-[0.5px] bg-slate-400 h-5 ml-3"></span>
   return (
     <header class="bg-primary box-content content-center items-center px-4 py-2 border-b-1 border-slate-300">
       <div class="my-auto flex h-8 items-center justify-between">
@@ -33,14 +31,16 @@ export function Header() {
             class={HEADER_BTN_CLASS}
           >
             <Link to="/">
-              <img src={icon} alt="logo"/>
+              <HamburgerMenuIcon class={"size-4 m-auto"} />
             </Link>
           </Button>
+          <Divider />
         </div>
         <SearchBar />
 
         {/* Right	*/}
         <div class="flex place-content-center items-center gap-3 shrink">
+          <Divider />
           <Show
             when={useUserCtx().user}
             fallback={<UnauthenticatedButtons />}
@@ -58,6 +58,9 @@ function AuthenticatedContent(props: { user: UserProfile }) {
   let close = () => setShow(false)
   return (
     <>
+      <div class="h-8 w-8 grid place-items-center">
+        <NotificationButton />
+      </div>
       <button onClick={() => setShow(!show())}>
         <Avatar
           user={props.user}
@@ -82,24 +85,20 @@ function AuthenticatedContent(props: { user: UserProfile }) {
   )
 }
 
-  function SearchBar() {
-    return (
-      <div class="flex justify-center">
-        <div class="relative w-96">
-          <Input
-            class="bg-slate-100 h-7 w-full rounded-xs duration-200 pl-2 pr-7 border-none"
-            placeholder="搜索车万云"
-          />
-          <MagnifyingGlassIcon
-            class="absolute right-2 top-1/2 -translate-y-1/2 size-4 text-gray-500"
-          />
-        </div>
-      </div>
-    );
-  }
-  
+function SearchBar() {
+  return (
+    <div class="grid items-center">
+      <Input
+        class={`
+        bg-slate-100 w-96 mr-auto h-7 rounded-xs duration-200 pl-2 border-none
+        `}
+      />
+      <MagnifyingGlassIcon class={"size-4 col-start-1 absolute ml-2"} />
+    </div>
+  )
+}
 
-/*function NotificationButton() { //按Figma上的示例图取消使用，保留代码供未来修改调用
+function NotificationButton() {
   let notification_state = createMemo(() => useUserCtx().notification_state)
   return (
     <Button
@@ -119,7 +118,7 @@ function AuthenticatedContent(props: { user: UserProfile }) {
       </Switch>
     </Button>
   )
-}*/
+}
 
 function UnauthenticatedButtons() {
   // @tw
