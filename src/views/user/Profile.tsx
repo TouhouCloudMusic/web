26c75c99c92a45a2824d6d53f59e1149d11f1d36
xiaochen17 <sinoces4097@gmail.com>
2025-04-10 11:10:45 +0800

    Profile页面的基础框架

--- src/views/user/Profile.tsx
+++ src/views/user/Profile.tsx
@@ -3,40 +3,40 @@ import { Match, Show, Switch } from "solid-js"
 import { PageLayout } from "~/components/layout/PageLayout"
 import { userProfileQuery } from "~/data/user"
 import { useUserCtx } from "~/state/user"
+import { Avatar } from "~/components/avatar"
+import { Button } from "~/components/button"
 
 export function Profile() {
   let params = useParams({ strict: false })()
   let query_result = userProfileQuery({ "params.username": params.username })
 
   return (
+
     <PageLayout>
+    <div class="col-span-12 col-start-7 flex flex-col items-center gap-4">
       <Switch>
         <Match when={query_result.isLoading}>
-          <div>Loading...</div>
-        </Match>
+          <p /><div class="text-xl font-bold text-center">少女祈祷中...</div>
+       </Match>
         <Match when={query_result.data}>
-          {(user) => (
-            <div class="flex justify-between">
-              <div>
-                <div>{user().name}</div>
-                <div>{user().last_login}</div>
-                <div>{user().roles}</div>
-              </div>
-              <Show when={useUserCtx().is_signed_in}>
-                <EditBtn />
-              </Show>
-            </div>
-          )}
-        </Match>
+        <Avatar user={query_result.data} class="size-24" />
+        <h2 class="text-xl font-bold">用户名：{query_result.data?.name}</h2>
+        <div class="text-xl font-bold">最后登录：{query_result.data?.last_login.slice(0, 16)}</div>
+        <EditBtn />
+       </Match>
       </Switch>
-    </PageLayout>
+      </div>
+    </PageLayout> 
+
   )
 }
 
 function EditBtn() {
   return (
     <div>
-      <Link to="/profile/edit">Edit</Link>
+      <Link to="/profile/edit">
+        <Button variant="Tertiary">编辑资料</Button>
+      </Link>
     </div>
   )
 }
