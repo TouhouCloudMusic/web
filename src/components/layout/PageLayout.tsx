49ab031db00fb22ea16a3f741f3d8e3e02c7a4a1
Huli <134658521+Huliiiiii@users.noreply.github.com>
2025-04-14 14:49:58 +0800

    Refactor `PageLayout`

--- src/components/layout/PageLayout.tsx
+++ src/components/layout/PageLayout.tsx
@@ -2,16 +2,25 @@ import { type ComponentProps, mergeProps, type ParentProps } from "solid-js"
 import { twMerge } from "tailwind-merge"
 
 export function PageLayout(props: ParentProps<ComponentProps<"div">>) {
-  const CLASS = `
+	const CLASS = `
+    bg-primary
     grid grid-cols-24
-    h-full w-2/3 max-w-5xl
+    h-full
+    lg:max-w-[min(max(75%,1024px),1280px)]
+    xl:max-w-3/4
+    2xl:max-w-7xl
     mx-auto
-    border-x-1 border-gray-300`
-  let final_props = mergeProps(props, {
-    get class() {
-      return twMerge(CLASS, props.class)
-    },
-  })
+    border-x-1 border-gray-300
+    `
+	let final_props = mergeProps(props, {
+		get class() {
+			return twMerge(CLASS, props.class)
+		},
+	})
 
-  return <div {...final_props}></div>
+	return (
+		<div class="size-full bg-slate-100">
+			<div {...final_props}></div>
+		</div>
+	)
 }
