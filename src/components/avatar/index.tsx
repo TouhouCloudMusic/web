ac41413daedab7e1a4b1afdd191ac08b5aab761c
Huli <134658521+Huliiiiii@users.noreply.github.com>
2025-04-16 00:23:06 +0800

    Change color of `Avatar`

--- src/components/avatar/index.tsx
+++ src/components/avatar/index.tsx
@@ -3,37 +3,37 @@ import { twMerge } from "tailwind-merge"
 import { type UserProfile } from "~/model/user"
 
 export interface Props
-  extends Omit<JSX.ImgHTMLAttributes<HTMLImageElement>, "src" | "onError"> {
-  user?: UserProfile | undefined
+	extends Omit<JSX.ImgHTMLAttributes<HTMLImageElement>, "src" | "onError"> {
+	user?: UserProfile | undefined
 }
 
 export function Avatar(props: Props) {
-  let [error, set_error] = createSignal(false)
+	let [error, set_error] = createSignal(false)
 
-  let [_, other_props] = splitProps(props, ["class", "user"])
+	let [_, other_props] = splitProps(props, ["class", "user"])
 
-  return (
-    <div class={twMerge("size-8 rounded-full overflow-hidden", props.class)}>
-      <Show
-        when={!error()}
-        fallback={
-          <div class="size-full flex items-center justify-center bg-gray-200">
-            <span class="text-gray-500">
-              {props.user?.name[0]?.toUpperCase() ?? "N/A"}
-            </span>
-          </div>
-        }
-      >
-        <img
-          {...other_props}
-          src={props.user?.avatar_url ?? ""}
-          alt={props.alt ?? "avatar"}
-          onError={() => {
-            set_error(true)
-          }}
-          class="object-cover size-full"
-        />
-      </Show>
-    </div>
-  )
+	return (
+		<div class={twMerge("size-8 overflow-hidden rounded-full", props.class)}>
+			<Show
+				when={!error()}
+				fallback={
+					<div class="flex size-full items-center justify-center bg-slate-200">
+						<span class="text-slate-500">
+							{props.user?.name[0]?.toUpperCase() ?? "N/A"}
+						</span>
+					</div>
+				}
+			>
+				<img
+					{...other_props}
+					src={props.user?.avatar_url ?? ""}
+					alt={props.alt ?? "avatar"}
+					onError={() => {
+						set_error(true)
+					}}
+					class="size-full object-cover"
+				/>
+			</Show>
+		</div>
+	)
 }
