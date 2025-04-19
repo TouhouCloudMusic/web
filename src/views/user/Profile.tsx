203cc11555d80b7b2aa57c2f42407e39278519d4
Huli <134658521+Huliiiiii@users.noreply.github.com>
2025-04-19 23:35:09 +0800

    Refactor

--- src/views/user/Profile.tsx
+++ src/views/user/Profile.tsx
@@ -1,6 +1,7 @@
 import { Link } from "@tanstack/solid-router"
 import {
 	type ComponentProps,
+	createContext,
 	Match,
 	mergeProps,
 	Show,
@@ -9,69 +10,88 @@ import {
 } from "solid-js"
 import { type DOMElement } from "solid-js/jsx-runtime"
 import { twMerge } from "tailwind-merge"
-import { url } from "valibot"
 import { Avatar } from "~/components/avatar"
 import { Button } from "~/components/button"
 import { PageLayout } from "~/components/layout/PageLayout"
 import { Data } from "~/data"
 import { type UserProfile } from "~/model/user"
+import { SafePick } from "~/types"
 import { imgUrl } from "~/utils/adapter/static_file"
+import { assertContext } from "~/utils/context"
 import { callHandlerUnion } from "~/utils/dom/event"
 
 type Props = {
 	data: Data<UserProfile>
 }
 
-export function Profile(props: Props) {
-	return (
-		<PageLayout>
-			{/* Background image */}
-			<div class="flex aspect-[4.236] w-full overflow-hidden bg-slate-200">
-				<Show when={props.data.value?.banner_url}>
-					{(url) => (
-						<img
-							src={imgUrl(url())}
-							class="size-full object-cover object-center"
-							alt="Banner of the user profile"
-						/>
-					)}
-				</Show>
-			</div>
+type Context = SafePick<Props, "data">
 
-			<div class="bg-white px-8 pb-8">
-				<div class="grid auto-rows-auto grid-cols-12 gap-4 px-4">
-					{/* Avatar */}
-					<div class="col-span-3 flex justify-center">
-						<div class="relative h-24 w-48">
-							<div class="absolute -top-25">
-								<Avatar
-									class="h-48 w-48 rounded-full border-3 border-white bg-slate-200"
-									user={props.data.value}
-								/>
+const Context = createContext<Context>()
+
+export function Profile(props: Props) {
+	const contextValue = {
+		get data() {
+			return props.data
+		},
+	}
+	return (
+		<Context.Provider value={contextValue}>
+			<PageLayout>
+				{/* Background image */}
+				<div class="flex h-68 w-full overflow-hidden bg-slate-200">
+					<Show when={props.data.value?.banner_url}>
+						{(url) => (
+							<img
+								src={imgUrl(url())}
+								class="size-full object-cover object-center"
+								alt="Banner of the user profile"
+							/>
+						)}
+					</Show>
+				</div>
+
+				<div class="bg-white px-8 pb-8">
+					<div class="grid auto-rows-auto grid-cols-12 gap-4 px-4">
+						{/* Avatar */}
+						<ProfileAvatar />
+						<div class="col-span-full col-start-4 flex h-fit rounded-xl px-2 py-4">
+							<div class="flex">
+								<span class="self-center font-inter text-xl font-semibold">
+									<Show when={Data.isOk(props.data)}>
+										{Data.unwrap(props.data).name}
+									</Show>
+								</span>
 							</div>
+							<FollowButton state={FollowButtonState.CurrentUser} />
 						</div>
-					</div>
-					<div class="col-span-full col-start-4 mb-4 flex h-fit rounded-xl px-2 py-4">
-						<div class="flex">
-							<span class="self-center font-inter text-xl font-semibold">
-								<Show when={Data.isOk(props.data)}>
-									{Data.unwrap(props.data).name}
-								</Show>
-							</span>
+						<div class="col-span-3 min-h-[1024px] rounded-xl bg-slate-100 p-4">
+							<h2 class="mb-2 font-bold text-slate-700">Intro</h2>
+							<p class="text-sm text-slate-600">简介内容...</p>
 						</div>
-						<FollowButton state={FollowButtonState.CurrentUser} />
-					</div>
-					<div class="col-span-3 min-h-[1024px] rounded-xl bg-slate-100 p-4">
-						<h2 class="mb-2 font-bold text-slate-700">Intro</h2>
-						<p class="text-sm text-slate-600">简介内容...</p>
-					</div>
-					<div class="col-span-9 rounded-xl bg-slate-100 p-4">
-						<h2 class="mb-2 font-bold text-slate-700">Timeline</h2>
-						<ul class="space-y-2 text-sm text-slate-600"></ul>
+						<div class="col-span-9 rounded-xl bg-slate-100 p-4">
+							<h2 class="mb-2 font-bold text-slate-700">Timeline</h2>
+							<ul class="space-y-2 text-sm text-slate-600"></ul>
+						</div>
 					</div>
 				</div>
+			</PageLayout>
+		</Context.Provider>
+	)
+}
+
+function ProfileAvatar() {
+	const context = assertContext(Context)
+
+	return (
+		<div class="col-span-3 -mt-[calc(40%+var(--avatar-border))] flex justify-center [--avatar-border:2px] md:[--avatar-border:3px] lg:[--avatar-border:4px]">
+			{/* The height must be fit to ensure that the avatar is round */}
+			<div class="flex aspect-square h-fit w-4/5 rounded-full bg-white">
+				<Avatar
+					class="m-auto h-fit w-[calc(100%-var(--avatar-border)*2)]"
+					user={context.data.value}
+				/>
 			</div>
-		</PageLayout>
+		</div>
 	)
 }
 
