19bedfdb20b3e77338eac03119ac71424f62fff6
Huli <134658521+Huliiiiii@users.noreply.github.com>
2025-04-21 01:28:10 +0800

    Fix imports

--- src/route/500.tsx
+++ src/route/500.tsx
@@ -1,18 +1,16 @@
 import { Title } from "@solidjs/meta"
-import { HttpStatusCode } from "@solidjs/start"
 
 export default function InternalServerError(props: {
-  msg?: string | undefined
+	msg?: string | undefined
 }) {
-  return (
-    <main class="w-[64rem] py-32">
-      <Title>Internal Server Error</Title>
-      <HttpStatusCode code={500} />
-      <img
-        src="/img/status_code/500.png"
-        alt="500 Internal Server Error"
-      />
-      <div>{props.msg}</div>
-    </main>
-  )
+	return (
+		<main class="w-[64rem] py-32">
+			<Title>Internal Server Error</Title>
+			<img
+				src="/img/status_code/500.png"
+				alt="500 Internal Server Error"
+			/>
+			<div>{props.msg}</div>
+		</main>
+	)
 }
