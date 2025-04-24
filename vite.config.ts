49558019ac29ae5313dc2fe3ae2704dd25dfe120
狐狸 <134658521+Huliiiiii@users.noreply.github.com>
2025-04-25 03:34:52 +0800

    Revert "Fix imports"
    This reverts commit 19bedfdb20b3e77338eac03119ac71424f62fff6.

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import suidPlugin from "@suid/vite-plugin"
 import tailwindcss from "@tailwindcss/vite"
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 import { defineConfig } from "vite"
@@ -8,32 +9,33 @@ import tsconfigPaths from "vite-tsconfig-paths"
 import { generatePlugin } from "./plugins/generate"
 
 export default defineConfig(({ mode }) => {
-	const SERVER_URL = loadEnv(mode, process.cwd())["VITE_SERVER_URL"]
+  const SERVER_URL = loadEnv(mode, process.cwd())["VITE_SERVER_URL"]
 
-	if (!SERVER_URL) {
-		throw new Error("Environment variable `VITE_SERVER_URL` is not defined")
-	}
-	return {
-		plugins: [
-			solidPlugin(),
-			tailwindcss(),
-			TanStackRouterVite({ target: "solid", autoCodeSplitting: true }),
-			tsconfigPaths(),
-			generatePlugin(SERVER_URL),
-		],
-		server: {
-			port: 3000,
-			proxy: {
-				"/api": {
-					target: SERVER_URL,
-					changeOrigin: true,
-					secure: false,
-					rewrite: (path) => path.replace(/^\/api/, ""),
-				},
-			},
-		},
-		build: {
-			target: "esnext",
-		},
-	}
+  if (!SERVER_URL) {
+    throw new Error("Environment variable `VITE_SERVER_URL` is not defined")
+  }
+  return {
+    plugins: [
+      solidPlugin(),
+      suidPlugin(),
+      tailwindcss(),
+      TanStackRouterVite({ target: "solid", autoCodeSplitting: true }),
+      tsconfigPaths(),
+      generatePlugin(SERVER_URL),
+    ],
+    server: {
+      port: 3000,
+      proxy: {
+        "/api": {
+          target: SERVER_URL,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+    build: {
+      target: "esnext",
+    },
+  }
 })
