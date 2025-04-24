07fdcf11ca7d2ed276ea094151df941e2bcff98a
狐狸 <134658521+Huliiiiii@users.noreply.github.com>
2025-04-25 03:34:52 +0800

    Revert "Add `repeated_password` field in sign up form"
    This reverts commit 806f12826360ee2c6885b71d113d194a3fdab139.

--- src/views/auth/index.tsx
+++ src/views/auth/index.tsx
@@ -1,22 +1,11 @@
 import {
-  type FieldElementProps,
-  type FieldStore,
+  createForm,
   FormError,
   valiForm,
-  Form,
-  Field,
-  createFormStore,
   type SubmitHandler,
-  type FormStore,
 } from "@modular-forms/solid"
 import { getRouteApi, Navigate, useNavigate } from "@tanstack/solid-router"
-import {
-  createEffect,
-  createMemo,
-  createSignal,
-  Show,
-  type ParentProps,
-} from "solid-js"
+import { createEffect, createSignal, Show, type ParentProps } from "solid-js"
 import * as v from "valibot"
 import { Button } from "~/components/button"
 import { Divider } from "~/components/divider"
@@ -24,33 +13,17 @@ import { Input } from "~/components/input"
 import { FetchClient } from "~/data"
 import { useUserCtx } from "~/state/user"
 
-const SignInForm = v.object({
+const AuthCredsSchema = v.object({
   username: v.string(),
   password: v.string(),
 })
-type SignInForm = v.InferInput<typeof SignInForm>
 
-const SignUpFrom = v.pipe(
-  v.object({
-    username: v.string(),
-    password: v.string(),
-    repeated_password: v.string(),
-  }),
-  v.forward(
-    v.partialCheck(
-      [["password"], ["repeated_password"]],
-      (input) => input.password === input.repeated_password,
-      "Password mismatch",
-    ),
-    ["repeated_password"],
-  ),
-)
-type SignUpForm = v.InferInput<typeof SignUpFrom>
+type SignInForm = v.InferInput<typeof AuthCredsSchema>
 
 export function Auth() {
   return (
     <Guard>
-      <FormComp />
+      <Form />
     </Guard>
   )
 }
@@ -68,26 +41,16 @@ function Guard(props: ParentProps) {
 
 const RouteApi = getRouteApi("/auth")
 
-function FormComp() {
-  type Mode = "sign_in" | "sign_up"
+function Form() {
+  let [sign_in_form, { Form, Field }] = createForm<SignInForm>({
+    validate: valiForm(AuthCredsSchema),
+  })
 
   let search_params = RouteApi.useSearch()
 
-  let [mode, setMode] = createSignal<Mode>(search_params().type)
-
-  const form_store = createMemo(() => {
-    if (mode() == "sign_in") {
-      // @ts-ignore
-      return createFormStore<SignInForm>({
-        validate: valiForm(SignInForm),
-      })
-    } else {
-      // @ts-ignore
-      return createFormStore<SignUpForm>({
-        validate: valiForm(SignUpFrom),
-      })
-    }
-  })
+  let [mode, setMode] = createSignal<"sign_in" | "sign_up">(
+    search_params().type,
+  )
 
   createEffect(() => {
     setMode(search_params().type)
@@ -125,12 +88,11 @@ function FormComp() {
   }
   return (
     <Form
-      of={form_store() as FormStore<SignInForm>}
       datatype="application/json"
       onSubmit={handle_submit}
-      class="mx-auto mt-32 flex w-1/3 flex-col gap-4 rounded border-1 border-slate-100 p-8 shadow-lg"
+      class="flex flex-col gap-4 w-1/3 mx-auto p-8 rounded shadow-lg border-1 border-slate-100 mt-32"
     >
-      <div class="mb-2 grid grid-cols-2 gap-2">
+      <div class="grid grid-cols-2 gap-2 mb-2">
         <Button
           variant={mode() === "sign_in" ? "Primary" : "Tertiary"}
           onClick={() => setMode("sign_in")}
@@ -148,51 +110,33 @@ function FormComp() {
         horizonal
         class="bg-slate-300"
       />
-      <Field
-        of={form_store() as FormStore<SignInForm>}
-        name="username"
-      >
+      <Field name="username">
         {(field, props) => (
-          <UserNameField
-            field={field}
-            props={props}
-          />
+          <div class="flex flex-col gap-2">
+            <label for="username">Username</label>
+            <Input
+              {...props}
+              type="text"
+              id="username"
+            />
+            {field.error && <div>{field.error}</div>}
+          </div>
         )}
       </Field>
-      <Field
-        of={form_store() as FormStore<SignInForm>}
-        name="password"
-      >
+      <Field name="password">
         {(field, props) => (
-          <PasswordField
-            name="password"
-            label="Password"
-            field={field}
-            props={props}
-          />
+          <div class="flex flex-col gap-2">
+            <label for="password">Password</label>
+            <Input
+              {...props}
+              type="password"
+              id="password"
+            />
+            {field.error && <div>{field.error}</div>}
+          </div>
         )}
       </Field>
-      <Show when={mode() === "sign_up"}>
-        <Field
-          of={form_store() as FormStore<SignUpForm>}
-          name="repeated_password"
-        >
-          {(field, props) => {
-            createEffect(() => {
-              console.log(field.error)
-            })
-            return (
-              <PasswordField
-                name="repeated_password"
-                label="Repeat Password"
-                field={field}
-                props={props}
-              />
-            )
-          }}
-        </Field>
-      </Show>
-      <FieldError>{form_store().response.message}</FieldError>
+      <div class="text-sm text-reimu-700">{sign_in_form.response.message}</div>
       <Divider
         horizonal
         class="bg-slate-300"
@@ -203,69 +147,8 @@ function FormComp() {
         color="Reimu"
         class="mt-4"
       >
-        {mode() === "sign_in" ? "Sign In" : "Sign Up"}
+        Sign In
       </Button>
     </Form>
-  )
-}
-
-function FieldLayout(
-  props: ParentProps<{
-    label: string
-    for: string
-    error?: string
-  }>,
-) {
-  return (
-    <div class="flex flex-col gap-2">
-      <label for={props.for}>{props.label}</label>
-      {props.children}
-      {props.error && <FieldError>{props.error}</FieldError>}
-    </div>
-  )
-}
-
-function FieldError(props: ParentProps) {
-  return <span class="text-sm text-reimu-700">{props.children}</span>
-}
-
-function UserNameField(props: {
-  field: FieldStore<SignInForm, "username">
-  props: FieldElementProps<SignInForm, "username">
-}) {
-  return (
-    <FieldLayout
-      label="Username"
-      for="username"
-      error={props.field.error}
-    >
-      <Input
-        {...props.props}
-        type="text"
-        id="username"
-      />
-    </FieldLayout>
-  )
-}
-
-function PasswordField<T extends "password" | "repeated_password">(props: {
-  name: T
-  label: string
-  field: T extends "password" ? FieldStore<SignInForm, T>
-  : FieldStore<SignUpForm, T>
-  props: FieldElementProps<SignUpForm, T>
-}) {
-  return (
-    <FieldLayout
-      label={props.label}
-      for={props.name}
-      error={props.field.error}
-    >
-      <Input
-        {...props.props}
-        type="password"
-        id={props.name}
-      />
-    </FieldLayout>
   )
 }
