d87f2802f8e127063c76571468851df0410ed851
狐狸 <134658521+Huliiiiii@users.noreply.github.com>
2025-04-25 03:34:52 +0800

    Revert "Refactor divider"
    This reverts commit 4ba36a38764eea7a4aec5ca9849a0a91802a2a05.

--- src/components/divider.tsx
+++ src/components/divider.tsx
@@ -1,24 +1,22 @@
 import { splitProps, type JSX } from "solid-js"
 import { twMerge } from "tailwind-merge"
 
-export type Props = (
-  | {
-      vertical: true
-      horizonal?: undefined
-    }
-  | {
-      vertical?: undefined
-      horizonal: true
-    }
-) &
-  JSX.HTMLAttributes<HTMLSpanElement>
+export type Props = {
+  direction: Direction
+} & JSX.HTMLAttributes<HTMLSpanElement>
+
+export const enum Direction {
+  Vertical,
+  Horizonal,
+}
 
 export function Divider(props: Props) {
   let direction_class = () => {
-    if (props.vertical) {
-      return "w-[0.5px] h-full"
-    } else {
-      return "h-[0.5px] w-full"
+    switch (props.direction) {
+      case Direction.Vertical:
+        return "w-[0.5px] h-full"
+      case Direction.Horizonal:
+        return "h-[0.5px] w-full"
     }
   }
 
