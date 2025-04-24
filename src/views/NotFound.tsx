596d8a596a3f5c639977bd0294c6cc5d82809b85
狐狸 <134658521+Huliiiiii@users.noreply.github.com>
2025-04-25 03:34:52 +0800

    Revert "Pretty not found page"
    This reverts commit 0562cb46d3ae8a93509eb9e218917782b21953f6.

--- src/views/NotFound.tsx
+++ src/views/NotFound.tsx
@@ -2,10 +2,9 @@ import { Title } from "@solidjs/meta"
 
 export function NotFound() {
   return (
-    <div class="size-full py-32 flex">
-      <Title>404 Not Found</Title>
+    <div class="w-[64rem] py-32">
+      <Title>Not Found</Title>
       <img
-        class="w-3/4 m-auto"
         src="/img/status_code/404.png"
         alt="404 Not Found"
       />
