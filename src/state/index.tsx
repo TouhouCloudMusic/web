95ab74b55abbb71087bbb5f77b9b91e92d9f592d
Huli <134658521+Huliiiiii@users.noreply.github.com>
2025-05-18 07:06:31 +0800

    Refactor context

--- src/state/index.tsx
+++ src/state/index.tsx
@@ -1,5 +1,5 @@
-import { MetaProvider } from "@solidjs/meta"
-import { type ParentProps } from "solid-js"
+import { MetaProvider } from "@solidjs/meta";
+import type { ParentProps } from "solid-js"
 
 import { I18NProvider } from "./i18n"
 import { TanStackProvider as QueryProvider } from "./tanstack"
@@ -7,15 +7,15 @@ import { ThemeProvider } from "./theme"
 import { UserContextProvider } from "./user"
 
 export function StateProvider(props: ParentProps) {
-  return (
-    <MetaProvider>
-      <QueryProvider>
-        <I18NProvider>
-          <UserContextProvider>
-            <ThemeProvider>{props.children}</ThemeProvider>
-          </UserContextProvider>
-        </I18NProvider>
-      </QueryProvider>
-    </MetaProvider>
-  )
+	return (
+		<MetaProvider>
+			<QueryProvider>
+				<I18NProvider>
+					<UserContextProvider>
+						<ThemeProvider>{props.children}</ThemeProvider>
+					</UserContextProvider>
+				</I18NProvider>
+			</QueryProvider>
+		</MetaProvider>
+	)
 }
