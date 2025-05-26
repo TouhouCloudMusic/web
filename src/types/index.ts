ba0e10605e0178ee4fff2a3ab1b191f5f1c7cf05
Huli <134658521+Huliiiiii@users.noreply.github.com>
2025-05-27 02:27:04 +0800

    Remove duplicate types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
+import type { If } from "./control_flow"
+
 // oxlint-disable no-magic-numbers
 export * from "./data"
 export * from "./control_flow"
@@ -26,8 +28,6 @@ export type Id<T> = T
 export type Extend<A, B> = A extends B ? true : false
 export type Eq<A, B> = Extend<A, B> & Extend<B, A>
 
-export type If<Cond extends boolean, T, F> = Cond extends true ? T : F
-
 type ControlFlowIf = {
 	if: boolean
 	then: unknown
@@ -36,44 +36,6 @@ type ControlFlowIf = {
 
 type ControlFlow<T extends ControlFlowIf> =
 	T["if"] extends true ? T["then"] : T["else"]
-
-export type Replace<T, A, B> = If<
-	IsRecord<T>,
-	{
-		[K in keyof T]: ControlFlow<{
-			if: IsRecord<T[K]>
-			then: Replace<T[K], A, B>
-			else: If<Eq<T[K], A>, B, T[K]>
-		}>
-	},
-	ReplaceInIntersection<T, A, B>
->
-
-type ReplaceInIntersection<T, From, To> = T extends infer U & From ? U & To : T
-
-type ReplaceTest1 = Replace<{ a: 1 }, 1, 2>
-const ReplaceTest1: ReplaceTest1 = { a: 2 }
-type ReplaceTest1Nested = Replace<
-	{
-		a: 1
-		b: {
-			c: 1
-		}
-	},
-	1,
-	2
->
-const ReplaceTest1Nested: ReplaceTest1Nested = {
-	a: 2,
-	b: {
-		c: 2,
-	},
-}
-
-type ReplaceTest2 = ReplaceInIntersection<number & Blob, Blob, File>
-const ReplaceTest2: ReplaceTest2 extends number & File ? true : false = true
-type ReplaceTest3 = ReplaceInIntersection<number | Blob, Blob, File>
-const ReplaceTest3: ReplaceTest3 extends number | File ? true : false = true
 
 export interface TypeError<T extends string> {
 	readonly __typeError__: T
