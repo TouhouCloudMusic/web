96cafe290c4768e8f8923702502bbfb92360695f
Huli <134658521+Huliiiiii@users.noreply.github.com>
2025-05-27 22:52:03 +0800

    Fix type def `Pagination`

--- src/api/shared/index.ts
+++ src/api/shared/index.ts
@@ -1,7 +1,8 @@
-import type { OpenApiSchema } from ".."
-
 // oxlint-disable-next-line export
 export * from "./schema"
 export * as DateWithPrecision from "./__internal/date_with_prec"
 
-export type Pagination = OpenApiSchema["Pagination"]
+export type Pagination = {
+	cursor: number
+	limit: number
+}
