b77579b1c18ec66cd8b57acdffdc3602f14e6bd6
Huli <134658521+Huliiiiii@users.noreply.github.com>
2025-07-16 21:38:12 +0800

    Refactor song tabs

--- src/views/song/Info/comp/SongInfoTabs.tsx
+++ src/views/song/Info/comp/SongInfoTabs.tsx
@@ -1,7 +1,9 @@
 /* @refresh skip */
+import { Trans } from "@lingui-solid/solid/macro"
 import { For } from "solid-js"
 
 import { Tab } from "~/components/common"
+import { Info } from "~/components/common/Info"
 import { CreditList } from "~/components/domain/credit"
 import { Image } from "~/components/image"
 import { groupSongCreditsByArtist } from "~/domain/song"
@@ -9,60 +11,50 @@ import { assertContext } from "~/utils/context"
 
 import { SongInfoPageContext } from ".."
 
-const TABS = [
-	"Releases",
-	"Credits",
-	// "Lyrics"
-] as const
-type Tabs = (typeof TABS)[number]
+// const TABS = [
+// 	"Releases",
+// 	"Credits",
+// 	// "Lyrics"
+// ] as const
+// type Tabs = (typeof TABS)[number]
 export function SongInfoTabs() {
 	const ctx = assertContext(SongInfoPageContext)
-	const visibleTabs = () =>
-		TABS.filter((tab) => {
-			switch (tab) {
-				case "Releases":
-					return ctx.song.releases && ctx.song.releases.length > 0
-				case "Credits":
-					return ctx.song.credits && ctx.song.credits.length > 0
-				// case "Lyrics":
-				// 	return ctx.song.lyrics
-				default:
-					return true
-			}
-		})
+	// const visibleTabs = () =>
+	// 	TABS.filter((tab) => {
+	// 		switch (tab) {
+	// 			case "Releases":
+	// 				return ctx.song.releases && ctx.song.releases.length > 0
+	// 			case "Credits":
+	// 				return ctx.song.credits && ctx.song.credits.length > 0
+	// 			// case "Lyrics":
+	// 			// 	return ctx.song.lyrics
+	// 			default:
+	// 				return true
+	// 		}
+	// 	})
 	return (
-		<Tab.Root>
-			<Tab.List>
-				<For each={visibleTabs()}>
-					{(tabType) => (
-						<li>
-							{/* TODO: Refactor this to a component */}
-							<Tab.Trigger
-								class="text-md size-full px-4 py-2.5 text-slate-800"
-								value={tabType}
-							>
-								{tabType}
-							</Tab.Trigger>
-						</li>
-					)}
-				</For>
-				<Tab.Indicator />
-			</Tab.List>
-			<Tab.Content<Tabs> value="Releases">
+		<div class="grid gap-x-8 gap-y-4 text-secondary lg:grid-cols-[1fr_auto]">
+			<div class="flex flex-col gap-4">
+				<h1 class="font-medium">
+					<Trans>Release</Trans>
+				</h1>
 				<ReleaseTab />
-			</Tab.Content>
-			<Tab.Content<Tabs> value="Credits">
-				<CreditTab />
-			</Tab.Content>
-		</Tab.Root>
+			</div>
+			<div class="flex flex-col gap-4">
+				<h1 class="font-medium">
+					<Trans>Credits</Trans>
+				</h1>
+				<CreditCard />
+			</div>
+		</div>
 	)
 }
 
-function CreditTab() {
+function CreditCard() {
 	const ctx = assertContext(SongInfoPageContext)
 
 	return (
-		<ul class="flex flex-col gap-4 py-4 pl-4">
+		<ul class="flex flex-col gap-4">
 			<CreditList
 				credits={
 					// This tab will hidden if credits is undefined or empty
@@ -72,11 +64,12 @@ function CreditTab() {
 		</ul>
 	)
 }
+
 function ReleaseTab() {
 	const ctx = assertContext(SongInfoPageContext)
 
 	return (
-		<div class="grid grid-cols-2 gap-4 py-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+		<div class="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
 			<For each={ctx.song.releases}>
 				{(release) => (
 					<div class="flex flex-col gap-2">
@@ -100,7 +93,8 @@ function ReleaseTab() {
 							</Image.Root>
 						</div>
 						<div class="text-center">
-							<p class="line-clamp-2 text-sm font-medium text-gray-900">
+							{/* TODO: Release Link */}
+							<p class="line-clamp-2 text-sm text-gray-1100 hover:underline">
 								{release.title}
 							</p>
 						</div>
