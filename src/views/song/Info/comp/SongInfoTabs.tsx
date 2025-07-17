b70e950b645042bc4eed0b522eec67fec9163191
Huli <134658521+Huliiiiii@users.noreply.github.com>
2025-07-17 19:27:18 +0800

    Add domain specific component `ReleaseCoverWall`

--- src/views/song/Info/comp/SongInfoTabs.tsx
+++ src/views/song/Info/comp/SongInfoTabs.tsx
@@ -1,9 +1,9 @@
 /* @refresh skip */
 import { Trans } from "@lingui-solid/solid/macro"
-import { For } from "solid-js"
+import { Show } from "solid-js"
 
 import { CreditList } from "~/components/domain/credit"
-import { Image } from "~/components/image"
+import { ReleaseCoverWall } from "~/components/domain/release/ReleaseCoverWall"
 import { SongCreditUtils } from "~/domain/song"
 import { assertContext } from "~/utils/context"
 
@@ -31,7 +31,7 @@ export function SongInfoTabs() {
 	// 		}
 	// 	})
 	return (
-		<div class="grid gap-x-8 gap-y-4 text-secondary lg:grid-cols-[1fr_auto]">
+		<div class="grid gap-x-8 py-4 text-secondary lg:grid-cols-[1fr_auto]">
 			<div class="flex flex-col gap-4">
 				<h1 class="font-medium">
 					<Trans>Release</Trans>
@@ -65,40 +65,13 @@ function CreditCard() {
 
 function ReleaseTab() {
 	const ctx = assertContext(SongInfoPageContext)
-
 	return (
-		<div class="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
-			<For each={ctx.song.releases}>
-				{(release) => (
-					<div class="flex flex-col gap-2">
-						<div class="aspect-square overflow-hidden">
-							<Image.Root>
-								<Image.Img
-									src={release.cover_art_url ?? undefined}
-									alt={release.title}
-									class="h-full w-full object-cover"
-								/>
-								<Image.Fallback>
-									{(state) =>
-										state !== Image.State.Ok && (
-											// TODO: Better fallback
-											<div class="flex h-full w-full items-center justify-center bg-gray-200">
-												<span class="text-sm text-gray-500">No Cover Art</span>
-											</div>
-										)
-									}
-								</Image.Fallback>
-							</Image.Root>
-						</div>
-						<div class="text-center">
-							{/* TODO: Release Link */}
-							<p class="line-clamp-2 text-sm text-gray-1100 hover:underline">
-								{release.title}
-							</p>
-						</div>
-					</div>
-				)}
-			</For>
-		</div>
+		<Show when={ctx.song.releases}>
+			{(releases) => (
+				<div class="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+					<ReleaseCoverWall releases={releases()} />
+				</div>
+			)}
+		</Show>
 	)
 }
