a2b88025317a99c2bfd9ca46862a1b95661108c2
Huli <134658521+Huliiiiii@users.noreply.github.com>
2025-08-25 01:18:30 +0800

    WIP

--- src/api/artist/type/artist_release.d.ts
+++
@@ -1,8 +0,0 @@
-import type { OpenApiSchema } from "~/api"
-
-export type Appearance =
-	OpenApiSchema["DataPaginatedAppearance"]["data"]["items"][number]
-export type Credit = OpenApiSchema["Paginated_Credit"]["items"][number]
-export type Discography =
-	OpenApiSchema["Paginated_Discography"]["items"][number]
-export type InitDiscography = OpenApiSchema["InitDiscography"]
