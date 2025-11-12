b55136faeab0b4bae8e80110fe2f60b6efc0eeb9
Yuyuko1024 <dingwenxuan4@gmail.com>
2025-11-12 09:19:03 +0800

    NotFound: Add notice text and resize image

--- src/view/NotFound.tsx
+++ src/view/NotFound.tsx
@@ -4,11 +4,20 @@ export function NotFound() {
 	return (
 		<div class="flex size-full py-32">
 			<Title>404 Not Found</Title>
-			<img
-				class="m-auto w-3/4"
-				src="/img/status_code/404.png"
-				alt="404 Not Found"
-			/>
+			<div class="m-auto text-center">
+				<img
+					class="m-auto w-1/3"
+					src="/img/status_code/404.png"
+					alt="404 Not Found"
+				/>
+				<h1 class="mt-4 text-4xl">404 Not Found</h1>
+				<p class="mt-2 text-slate-600">
+					The page you are looking for does not exist.
+				</p>
+				<p class="mt-1 text-slate-400">
+					Hey you, lost one. why you are here? <a class="text-blue-400 hover:underline" href="/">You should go back...</a>
+				</p>
+			</div>
 		</div>
 	)
 }
