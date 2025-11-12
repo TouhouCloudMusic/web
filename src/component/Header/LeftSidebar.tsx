dc8eda844248edb9f397bf4037226ca46787d0c7
Yuyuko1024 <dingwenxuan4@gmail.com>
2025-11-12 08:50:57 +0800

    LeftSidebar: Add configurable router click event

--- src/component/Header/LeftSidebar.tsx
+++ src/component/Header/LeftSidebar.tsx
@@ -11,49 +11,61 @@ import {
 	CrumpledPaperIcon,
 	HomeIcon,
 } from "solid-radix-icons"
+import { useNavigate } from "@tanstack/solid-router"
 
 import { ListItem, Sidebar } from "~/component/Sidebar"
+import { cons } from "effect/List"
 
 type ListItemContent = {
 	icon: (props: IconProps) => JSX.Element
-	text: string
+	text: string,
+	to: string,
 }
 
 export function LeftSidebar() {
 	const { t } = useLingui()
+	const navigate = useNavigate()
 	// TODO: Icons
 	const LIST_ITEMS: ListItemContent[] = [
 		{
 			icon: HomeIcon,
 			text: t`Home`,
+			to: "/",
 		},
 		{
 			icon: TargetIcon,
 			text: t`Recommandation`,
+			to: "/recommendation",
 		},
 		{
 			icon: RankingIcon,
 			text: t`Chart`,
+			to: "/chart",
 		},
 		{
 			icon: CardStackIcon,
 			text: t`Song`,
+			to: "/song",
 		},
 		{
 			icon: CrumpledPaperIcon,
 			text: t`Release`,
+			to: "/release",
 		},
 		{
 			icon: MixerHorizontalIcon,
 			text: t`Artist`,
+			to: "/artist",
 		},
 		{
 			icon: EnvelopeClosedIcon,
 			text: t`Event`,
+			to: "/event",
 		},
 		{
 			icon: EnvelopeClosedIcon,
 			text: t`Tag`,
+			to: "/tag",
 		},
 	]
 
@@ -68,7 +80,7 @@ export function LeftSidebar() {
 					<For each={LIST_ITEMS}>
 						{(item) => {
 							return (
-								<ListItem class="w-full">
+								<ListItem class="w-full" onClick={() => navigate({ to: item.to })}>
 									<item.icon class="mr-3 h-4 w-4" />
 									<span>{item.text}</span>
 								</ListItem>
